e69bc539ccd0f0ab3a762be0c0db48f99660efa1
kimgura <oli999@naver.com>
2020-12-29 11:06:07 +0900

    Step03_Scope 에 서블릿에서 HttpSession 사용하는 예제 추가함

---
+++ Step03_Scope/src/test/servlet/SaveServlet.java
@@ -0,0 +1,37 @@
+package test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/test/save")
+public class SaveServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		//폼 전송되는 닉네임 읽어오기
+		String nick=req.getParameter("nick");
+		//session scope 에 저장하기 (어떻게 저장하지?)
+		HttpSession session=req.getSession(); //HttpServletRequest 객체를 이용해서 얻어올수 있다.
+		session.setAttribute("nick", nick);
+		//간단 응답하기
+		PrintWriter pw=resp.getWriter();
+		pw.println("nick save ok!");
+		pw.close();
+	}
+}
+
+
+
+
+
+
+
+
+
