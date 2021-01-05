48ccef77c5bc7332723946d320389bb4b1bfa90a
kimgura <oli999@naver.com>
2021-01-05 11:55:27 +0900

    Step06 로그인 기능 구현함

--- Step06_Final/src/test/users/dao/UsersDao.java
+++ Step06_Final/src/test/users/dao/UsersDao.java
@@ -2,6 +2,7 @@ package test.users.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import test.users.dto.UsersDto;
 import test.util.DbcpBean;
@@ -18,6 +19,48 @@ public class UsersDao {
 		}
 		return dao;
 	}
+	//인자로 전달된 정보가 유효한 정보인지 여부를 리턴하는 메소드
+	public boolean isValid(UsersDto dto) {
+		//아이디 비밀번호가 유효한 정보인지 여부를 담을 지역변수 만들고 초기값 부여
+		boolean isValid=false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT id"
+					+ " FROM users"
+					+ " WHERE id=? AND pwd=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			if(rs.next()) {//만일 select 된 row 가 있다면
+				//유효한 정보임으로 isValid 에 true 를 대입한다. 
+				isValid=true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		//아이디가 유효한지 여부를 리턴해준다. 
+		return isValid;
+	}//isValid()
+	
 	//회원 정보를 저장하는 메소드
 	public boolean insert(UsersDto dto) {
 		Connection conn = null;
