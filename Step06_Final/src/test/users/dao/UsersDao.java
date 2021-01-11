79adc4e4075921b5cc582a278fc3d4c012290a33
likuimin04 <likuimin04@outlook.com>
2021-01-11 09:46:24 +0900

    21.01.11 오전09:46

--- Step06_Final/src/test/users/dao/UsersDao.java
+++ Step06_Final/src/test/users/dao/UsersDao.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
-
 import test.users.dto.UsersDto;
 import test.util.DbcpBean;
 
@@ -21,41 +19,80 @@ public class UsersDao {
 		}
 		return dao;
 	}
-	
-	//회원의 비밀번호를 수정하는 메소드
-		public boolean updatePwd(UsersDto dto) {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			int flag = 0;
-			try {
-				conn = new DbcpBean().getConn();
-				//실행할 insert, update, delete 문 구성
-				String sql = "UPDATE users"
-						+ " SET pwd=?"
-						+ " WHERE id=? AND pwd=?";
-				pstmt = conn.prepareStatement(sql);
-				//? 에 바인딩할 내용이 있으면 바인딩한다.
-				pstmt.setString(1, dto.getNewPwd()); //새 비밀번호
-				pstmt.setString(2, dto.getId());
-				pstmt.setString(3, dto.getPwd()); //구 비밀번호
-				flag = pstmt.executeUpdate(); //sql 문 실행하고 변화된 row 갯수 리턴 받기
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (pstmt != null)
-						pstmt.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e) {
-				}
+	//인자로 전달된 아이디가 DB 에 존재하는지 여부를 리턴하는 메소드
+	public boolean isExist(String id) {
+		//아이디가 이미 존재하는지 여부를 담을 지역변수 선언하고 초기값 지정 
+		boolean isExist=false;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT * "
+					+ " FROM users"
+					+ " WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setString(1, id);
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			if (rs.next()) {
+				isExist=true;
 			}
-			if (flag > 0) {
-				return true;
-			} else {
-				return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
 			}
 		}
+		//아이디가 이미 존재하는지 여부를 리턴해준다.
+		return isExist;
+	}
+	
+	//회원의 비밀번호를 수정하는 메소드
+	public boolean updatePwd(UsersDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 insert, update, delete 문 구성
+			String sql = "UPDATE users"
+					+ " SET pwd=?"
+					+ " WHERE id=? AND pwd=?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 바인딩한다.
+			pstmt.setString(1, dto.getNewPwd()); //새 비밀번호
+			pstmt.setString(2, dto.getId());
+			pstmt.setString(3, dto.getPwd()); //구 비밀번호
+			flag = pstmt.executeUpdate(); //sql 문 실행하고 변화된 row 갯수 리턴 받기
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	//회원 가입 정보를 수정반영하는 메소드
 	public boolean update(UsersDto dto) {
@@ -92,40 +129,40 @@ public class UsersDao {
 	}
 	
 	//인자로 전달된 아이디에 해당하는 가입정보를 삭제하는 메소드
-		public boolean delete(String id) {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			int flag = 0;
+	public boolean delete(String id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 insert, update, delete 문 구성
+			String sql = "DELETE FROM users"
+					+ " WHERE id=?";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 바인딩할 내용이 있으면 바인딩한다.
+			pstmt.setString(1, id);
+			flag = pstmt.executeUpdate(); //sql 문 실행하고 변화된 row 갯수 리턴 받기
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
 			try {
-				conn = new DbcpBean().getConn();
-				//실행할 insert, update, delete 문 구성
-				String sql = "DELETE FROM users"
-						+ " WHERE id=?";
-				pstmt = conn.prepareStatement(sql);
-				//? 에 바인딩할 내용이 있으면 바인딩한다.
-				pstmt.setString(1, id);
-				flag = pstmt.executeUpdate(); //sql 문 실행하고 변화된 row 갯수 리턴 받기
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
 			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (pstmt != null)
-						pstmt.close();
-					if (conn != null)
-						conn.close();
-				} catch (Exception e) {
-				}
-			}
-			if (flag > 0) {
-				return true;
-			} else {
-				return false;
 			}
 		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	//인자로 전달된 아이디에 해당하는 가입정보를 리턴해주는 메소드
 	public UsersDto getData(String id) {
-		//회원 정보를 담을 UsersDto
+		//회원 정보를 담을 UsersDto 
 		UsersDto dto=null;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -166,9 +203,10 @@ public class UsersDao {
 		return dto;
 	}
 	
+	
 	//인자로 전달된 정보가 유효한 정보인지 여부를 리턴하는 메소드
 	public boolean isValid(UsersDto dto) {
-		// 아이디 비밀번호가 유효한 정보인지 여부를 담을 지역변수 만들고 초기값 부여
+		//아이디 비밀번호가 유효한 정보인지 여부를 담을 지역변수 만들고 초기값 부여
 		boolean isValid=false;
 		
 		Connection conn = null;
@@ -187,8 +225,8 @@ public class UsersDao {
 			//select 문 수행하고 ResultSet 받아오기
 			rs = pstmt.executeQuery();
 			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
-			if(rs.next()) { //만일 select 된 row 가 있다면
-				//유효한 정보임으로 isValid에 true 를 대입한다.
+			if(rs.next()) {//만일 select 된 row 가 있다면
+				//유효한 정보임으로 isValid 에 true 를 대입한다. 
 				isValid=true;
 			}
 		} catch (Exception e) {
@@ -204,9 +242,9 @@ public class UsersDao {
 			} catch (Exception e) {
 			}
 		}
-		//아이디가 유효한지 여부를 리턴해준다.
+		//아이디가 유효한지 여부를 리턴해준다. 
 		return isValid;
-	}
+	}//isValid()
 	
 	//회원 정보를 저장하는 메소드
 	public boolean insert(UsersDto dto) {
