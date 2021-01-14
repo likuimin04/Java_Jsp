34e18c46d2082ddde4ae4fb8614f6f374c4c9102
kimgura <oli999@naver.com>
2021-01-14 15:58:16 +0900

    Step06  gallery 자세히보기 페이지 구현중...

---
+++ Step06_Final/WebContent/gallery/detail.jsp
@@ -0,0 +1,31 @@
+<%@page import="test.gallery.dao.GalleryDao"%>
+<%@page import="test.gallery.dto.GalleryDto"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+	//자세히 보여줄 겔러리 item 번호를 읽어온다.
+	int num=Integer.parseInt(request.getParameter("num"));
+	//번호를 이용해서 겔러리 item 정보를 얻어온다.
+	GalleryDto dto=GalleryDao.getInstance().getData(num);
+%>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>/gallery/detail.jsp</title>
+</head>
+<body>
+<div class="container">
+	<div>
+		<img src="${pageContext.request.contextPath }<%=dto.getImagePath()%>"/>
+		<div>
+			<p><%=dto.getCaption() %></p>
+			<p>by <strong><%=dto.getWriter() %></strong></p>
+			<p><small><%=dto.getRegdate() %></small></p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
+
+
--- Step06_Final/src/test/gallery/dao/GalleryDao.java
+++ Step06_Final/src/test/gallery/dao/GalleryDao.java
@@ -18,6 +18,47 @@ public class GalleryDao {
 		}
 		return dao;
 	}
+	public GalleryDto getData(int num) {
+		GalleryDto dto=null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//select 문 작성
+			String sql = "SELECT writer,caption,imagePath,regdate"
+					+ " FROM board_gallery"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩 할게 있으면 여기서 바인딩한다.
+			pstmt.setInt(1, num);
+			//select 문 수행하고 ResultSet 받아오기
+			rs = pstmt.executeQuery();
+			//while문 혹은 if문에서 ResultSet 으로 부터 data 추출
+			if(rs.next()) {
+				dto=new GalleryDto();
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setCaption(rs.getString("caption"));
+				dto.setImagePath(rs.getString("imagePath"));
+				dto.setRegdate(rs.getString("regdate"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}
+	
 	public int getCount() {
 		int count=0;
 		Connection conn = null;
